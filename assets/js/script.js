186974111af0aca6e404beae48221b5fbe8434d7
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-03-19 19:13:16 -0400

    added more content to index.html, added more styling, including styling for different breakpoints, and added some javascript for navbar experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,39 @@ $(document).ready(function () {
       $('.animated-icon').toggleClass('open');
     });
 
-    // TODO let's get some gsap animations in here 
+    // event handlers for collapsing navbar menu when links are clicked 
+    $('.about-link').on('click', function () {
+      $('.animated-icon').toggleClass('open');
+      $('.navbar-collapse').removeClass('show')
+    });
+
+    $('.projects-link').on('click', function () {
+      $('.animated-icon').toggleClass('open');
+      $('.navbar-collapse').removeClass('show')
+    });
+
+    $('.resume-link').on('click', function () {
+      $('.animated-icon').toggleClass('open');
+      $('.navbar-collapse').removeClass('show')
+    });
+
+    // navbar scroll behavior
+    let prevScrollPos = window.pageYOffset;
+    window.onscroll = function() {
+      let currentScrollPos = window.pageYOffset;
+      if (prevScrollPos > currentScrollPos) {
+        $('.custom-nav').css('top', '0');
+      } else {
+        $('.custom-nav').css('top', '-50px');
+      }
+      prevScrollPos = currentScrollPos
+    }
+
+    // gsap animation for .custom-jumbo
     gsap.from(".my-name", {duration: 3.5, opacity: 0, scale: 0.01, ease: "back"});
     gsap.from(".web-dev", {duration: 1, opacity: 0, y: 150, delay: 2.5});
 
+
+
   });
+
