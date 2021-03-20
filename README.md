f8c030e7ba54f8a7d2a653bf8fb544476a957571
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-03-20 19:36:48 -0400

    added screenshots to project buttons, modified script for navbar behavior to fix a bug where navbar disappeared when burger icon clicked while user scrolled down page, did some minor styling, added content to README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# ian-everitt-portfolio
+# My Portfolio
+This is my personal portfolio. I've linked five of the projects I'm proudest of, my resume, and different ways to contact me (of course, the phone number I have listed is not my own). 
+
+I have a number of future plans to improve the site. For example, I'd like to include more animations to enrich the UX, I'd like to use icons for my project buttons (rather than screenshots of the projects themselves), and I'd like to make the Contact section more like a footer rather than a dedicated section of the page. Furthermore, I'd like to dedicate a section in my portfolio to my philosophical endeavors, but I haven't yet found the best way to do that, so this section is currently more of a placeholder than anything. 
+
+## Deployment 
+To visit this website, simply follow this [link](https://rflctveq.github.io/ian-everitt-portfolio/) or enter the following into your address bar: https://rflctveq.github.io/ian-everitt-portfolio/. 
+
+## Technologies Used 
+* Bootstrap 4.6.0
+
+* jQuery 3.5.1 
+
+* GreenSock Animation Platform 3.6.0 
+
+* GitHub Pages (host)
+
+## Screenshots
+Here's are a couple images of my portfolio as seen on a desktop:
+
+![desktop-splash](assets/images/desktop-ss-jumbo.png)
+
+![desktop-homepage](assets/images/desktop-ss-about.png)
+
+And here's an image as seen on an iPhone X: 
+
+![mobile-splash](assets/images/mobile-ss-jumbo.png)
+
+## Contact 
+### Ian Everitt 
+* Email me: ian.andrew.everitt@gmail.com 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     // event handler for hamburger icon animation
-    $('.second-button').on('click', function () {
-  
+    $('.second-button').on('click', function() {
+
       $('.animated-icon').toggleClass('open');
     });
 
     // event handlers for collapsing navbar menu when links are clicked 
-    $('.about-link').on('click', function () {
+    $('.about-link').on('click', function() {
       $('.animated-icon').toggleClass('open');
       $('.navbar-collapse').removeClass('show')
     });
 
-    $('.projects-link').on('click', function () {
+    $('.projects-link').on('click', function() {
       $('.animated-icon').toggleClass('open');
       $('.navbar-collapse').removeClass('show')
     });
 
-    $('.resume-link').on('click', function () {
+    $('.resume-link').on('click', function() {
       $('.animated-icon').toggleClass('open');
       $('.navbar-collapse').removeClass('show')
     });
@@ -28,6 +28,9 @@ $(document).ready(function () {
       let currentScrollPos = window.pageYOffset;
       if (prevScrollPos > currentScrollPos) {
         $('.custom-nav').css('top', '0');
+      // this ensures that the navbar doesn't disappear when the user clicks on the hamburger icon
+      } else if ($('.animated-icon').hasClass('open') === true) {
+        $('.custom-nav').css('top', '0');
       } else {
         $('.custom-nav').css('top', '-50px');
       }
@@ -37,8 +40,4 @@ $(document).ready(function () {
     // gsap animation for .custom-jumbo
     gsap.from(".my-name", {duration: 3.5, opacity: 0, scale: 0.01, ease: "back"});
     gsap.from(".web-dev", {duration: 1, opacity: 0, y: 150, delay: 2.5});
-
-
-
   });
-
